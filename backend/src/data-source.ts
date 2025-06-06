3024cb415e7e32bc1706acc99bbd468a89c99445
Hydra5120 <s4062985@student.rmit.edu.au>
2025-06-06 15:13:48 +1000

    changed data source to connect to the reuired db

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -6,9 +6,9 @@ export const AppDataSource = new DataSource({
   type: "mysql",
   host: "209.38.26.237",
   port: 3306,
-  username: "matt",
-  password: "matt",
-  database: "matt",
+  username: "S4062985",
+  password: "4062985",
+  database: "S4062985",
   // synchronize: true will automatically create database tables based on entity definitions
   // and update them when entity definitions change. This is useful during development
   // but should be disabled in production to prevent accidental data loss.
